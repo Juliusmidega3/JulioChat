203bcbc0df299c64bb640f83d59036d2f23ba1e5
Julius <midegajulius3@gmail.com>
2025-02-04 13:51:48 +0300

    profile

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Card from "./components/card/Card"
+import Logo from "./components/logo/Logo"
 
 const App = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState("");
 
   return (
-    <div className="container">
+    <div>
+      <Logo />
+      <div className="container">
       {user ? (
         <>
+        <Logo />
         <Navbar />
         <Card />
         <span className="username">{user}</span>
@@ -24,6 +28,7 @@ const App = () => {
           <button onClick={() => setUser(username)}>Login</button>
         </div>
       )}
+    </div>
     </div>
   )
 }
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,14 @@
+import "./profile.css"
+import Julio from "../../img/JulioChat.svg"
+
+const Profile = () =>  {
+    return (
+        <div className="container">
+            <img src={Julio} alt="" className="imgCont" />
+        </div>
+    )
+}
+
+
+
+export default Profile
