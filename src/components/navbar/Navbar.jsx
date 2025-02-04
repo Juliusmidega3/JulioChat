c81a8dc0f826056c31248201431b49065bd71fb4
Julius <midegajulius3@gmail.com>
2025-02-04 12:14:15 +0300

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./navbar.css"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <span className="logo">Julio Chat</span>
+            <div className="icons">
+                <div className="icon">
+                    <img src="" alt="" className="iconImg" />
+                    <div className="counter">2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
